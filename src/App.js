6f8c41db82f246308b1eecf1459b84337b1676e6
bdnkhanh <buidunamkhanh4126@gmail.com>
2022-12-08 07:57:11 +0700

    bdnkhanh commit code solidity

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Button from 'react-bootstrap/Button';
+
+const App = () => {
+  return (
+    <center>
+      <Button variant="primary">bdnkhanh</Button>
+    </center>
+  );
+};
+
+export default App;
