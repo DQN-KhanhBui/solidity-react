e20ece689da99014160be75d0d9faa577947f167
Khanh Bui <buidunamkhanh4126@gmail.com>
2022-12-09 14:53:09 +0700

    check metamask

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -2,6 +2,47 @@
 pragma solidity 0.8.17;
 
 contract Wallet {
-  uint256 public numOfFunders;
-  
+  uint256 public numOfFunders; // SO LUONG NGUOI DONATE
+  mapping(uint256 => address) public funders; // MANG CHUA ADDRESS WALLET DA DONATE
+  mapping(address => bool) public isFunders; // CHECK ADDRESS DA DONATE CHUA???
+
+  receive() external payable {}
+
+  /*
+  ** addFunder()
+  ** getFunderIndex(uint256 index) => returns(address)
+  ** getAllFunder()                => returns(address[] memory)
+  ** modify limitWithdraw(uint256 withdrawAmout)
+  ** withdraw(uint256 withdrawAmount)
+  */
+
+  function addFunder() external payable {
+    address funder = msg.sender;
+    if(!isFunders[funder]) {
+      uint index = numOfFunders++;
+      isFunders[funder] = true;
+      funders[index] = funder;
+    }
+  }
+
+  function getFunderIndex(uint256 index) external view returns(address) {
+    return funders[index];
+  }
+
+  function getAllFunder() external view returns(address[] memory) {
+    address[] memory _funders = new address[](numOfFunders);
+    for(uint256 i=0; i<numOfFunders; i++) {
+      _funders[i] = funders[i];
+    }
+    return _funders;
+  }
+
+  modifier limitWithdraw(uint256 withdrawAmount) {
+    require(withdrawAmount <= 1*(10**18), "Can't withdraw more than 1ETH");
+    _;
+  }
+
+  function withdraw(uint256 withdrawAmount) external limitWithdraw(withdrawAmount) {
+    payable(msg.sender).transfer(withdrawAmount);
+  }
 }
--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
 
 const App = () => {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  });
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        provider.request({method: "eth_requestAccounts"});
+        // setWeb3Api({
+        //   provider: provider,
+        //   web3: new Web3(provider)
+        // })
+      } else {
+        console.log("Pls, install metamask!");
+      }
+    }
+    loadProvider();
+  }, []);
+
   return (
     <React.Fragment>
       <Card className="text-center mx-5 mt-5">
